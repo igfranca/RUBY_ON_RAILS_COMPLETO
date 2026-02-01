f11399669ee5f2281aba15f31b0a2ce29164f45b
igfranca <igrfranca@gmail.com>
2026-02-01 19:59:44 -0300

    Condicionais

---
+++ Curso/modulo_02/03_condicionais.rb
@@ -0,0 +1,47 @@
+# CONDICIONAIS TERNÁRIAS
+
+sexo = 'M'
+
+if sexo == 'M'
+  puts 'Masculino'
+else
+  puts 'Feminino'
+end
+
+sexo == 'M' ? puts('Masculino') : puts('Feminino')
+
+
+# CASE
+print 'Digite uma idade: '
+idade = gets.chomp.to_i
+
+case idade
+when 0..2
+  puts 'Bebê'
+when 3..12
+  puts 'Criança'
+when 13..18
+  puts 'Adolescente'
+else
+  puts 'Adulto'
+end
+
+
+# UNLESS, Lógica invertida do IF
+print 'Digite um número: '
+x = gets.chomp.to_i
+
+unless x >= 2
+  puts 'x é menor que 2'
+else
+  puts 'x é maior que 2'
+end
+
+
+# IF
+print 'Digite um número: '
+x = gets.chomp.to_i
+
+if x > 2
+  puts 'x é maior que 2'
+end
